2bcef5c0ec0c9c8c534fbc0e1f1f2a53539a69ad
Swapnil Pote <138121134+poteswapnil555@users.noreply.github.com>
2024-10-10 15:56:47 +0530

    Add files via upload

---
+++ ChatApp/src/firebase.jsx
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "I can't share personal information here",
+  authDomain: "I can't share personal information here",
+  projectId: "I can't share personal information here",
+  storageBucket: "I can't share personal information here",
+  messagingSenderId: "I can't share personal information here",
+  appId: "I can't share personal information here",
+};
+
+export const app = initializeApp(firebaseConfig);
